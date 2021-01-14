f66b87e382941a1e5d0453436d513b8f5fa91f0c
Peter Adegboyega <peter.gboyega@yahoo.com>
2021-01-14 16:42:28 +0100

    My Project Setup Completed Successfully

---
+++ shoppinghubbackend/src/main/java/net/pet/shoppinghubbackend/App.java
@@ -0,0 +1,13 @@
+package net.pet.shoppinghubbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
